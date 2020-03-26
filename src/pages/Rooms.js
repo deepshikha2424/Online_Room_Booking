c6396c4421a424fed3203b99a410996a1b5d19d8
deepshikha2424 <deepshikha.vaishnav@sigmainfo.net>
2020-03-26 18:29:51 +0530

    changes in Banner.js

---
+++ src/pages/Rooms.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import RoomContainer from '../components/RoomContainer';
+
+const Rooms = () => {
+    return  (
+    <>
+    <Hero hero="roomsHero">
+        <Banner title="Our Rooms">
+            <Link to="/rooms" className="btn-primary">
+                Return Home
+            </Link>
+        </Banner>
+    </Hero>
+    <RoomContainer></RoomContainer>
+    </>
+    );
+}
+
+
+
+export default Rooms;
